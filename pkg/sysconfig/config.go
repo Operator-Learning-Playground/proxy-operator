30c8c5503d90b007d3022ea2e7d43a0ff9643ac9
googs1025 <googs1025@gmail.com>
2023-03-10 22:17:53 +0800

    feat: 新增query 参数限流中间件功能

--- pkg/sysconfig/config.go
+++ pkg/sysconfig/config.go
@@ -76,8 +76,9 @@ type Backend struct {
 }
 
 type Server struct {
-	Ip   string `yaml:"ip"`
-	Port int 	`yaml:"port"`
+	Ip     string `yaml:"ip"`
+	Port   int 	  `yaml:"port"`
+	Params string `yaml:"params"`
 }
 
 func AppConfig(proxy *proxyv1alpha1.Proxy) error {
